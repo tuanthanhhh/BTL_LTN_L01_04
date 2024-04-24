d8c21db450d3f338a4b47d29f34c6fafca9e4665
CaoAnhMinhBK2003 <158066166+CaoAnhMinhBK2003@users.noreply.github.com>
2024-04-24 14:44:39 +0700

    LM393.c

--- Core/SENSOR/LM393.c
+++ Core/SENSOR/LM393.c
@@ -9,7 +9,7 @@ void LM393_Init(ADC_HandleTypeDef *hadc) {
 float LM393_ReadHumidity(void) {
     uint16_t adc_value = 0;
     float humidity = 0.0;
-
+    float voltage = 0.0;
     // Start ADC conversion
     HAL_ADC_Start(hadc_ptr);
 
@@ -23,7 +23,8 @@ float LM393_ReadHumidity(void) {
     HAL_ADC_Stop(hadc_ptr);
 
     // Convert ADC value to soil humidity (calibration dependent)
-    humidity = ((float)adc_value / 4095.0f) * 100.0f;
+    voltage = ((float)adc_value / 4095.0f) * 100.0f;
+    humidity = 100.0f * (voltage / 3.0f);
 
     return humidity;
 }
